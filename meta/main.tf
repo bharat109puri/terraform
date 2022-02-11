c62d9a38ce3a78bf51013c1a62aea41436c3f28f
Mate Farkas <mate@recrd.com>
2022-02-11 20:24:41 +0000

    Add meta workspace

---
+++ meta/main.tf
@@ -0,0 +1,33 @@
+locals {
+  members = toset([
+    "gabor@recrd.com",
+    "mate@recrd.com",
+  ])
+}
+
+data "tfe_team" "owners" {
+  name         = "owners"
+  organization = tfe_organization.recrd.name
+}
+
+resource "tfe_organization" "recrd" {
+  name  = "recrd"
+  email = "mate@recrd.com" # FIXME
+
+  #collaborator_auth_policy = "two_factor_mandatory" # TODO: Ask Gabor to enable 2FA
+}
+
+resource "tfe_organization_membership" "recrd" {
+  for_each = local.members
+
+  organization = tfe_organization.recrd.name
+  email        = each.value
+}
+
+resource "tfe_team_organization_member" "owners" {
+  # NOTE: In Terraform Cloud Free everyone is owner
+  for_each = local.members
+
+  team_id                    = data.tfe_team.owners.id
+  organization_membership_id = tfe_organization_membership.recrd[each.value].id
+}
---
+++ meta/workspaces.tf
@@ -0,0 +1,39 @@
+resource "tfe_workspace" "meta" {
+  name         = "meta"
+  organization = tfe_organization.recrd.name
+
+  description = "Managing Terraform Cloud"
+
+  allow_destroy_plan    = false
+  auto_apply            = false
+  execution_mode        = "local" # FIXME: remote? (tfe_organization requires user token)
+  file_triggers_enabled = false
+  queue_all_runs        = false
+  trigger_prefixes      = ["meta"]
+  working_directory     = "meta"
+
+  #vcs_repo {
+  #  identifier     =
+  #  oauth_token_id =
+  #}
+}
+
+resource "tfe_workspace" "users" {
+  name         = "users"
+  organization = tfe_organization.recrd.name
+
+  description = "AWS IAM users and roles"
+
+  allow_destroy_plan    = false
+  auto_apply            = false
+  execution_mode        = "local" # FIXME: AWS credentials
+  file_triggers_enabled = true
+  queue_all_runs        = true
+  trigger_prefixes      = ["users"]
+  working_directory     = "users"
+
+  #vcs_repo {
+  #  identifier     =
+  #  oauth_token_id =
+  #}
+}
