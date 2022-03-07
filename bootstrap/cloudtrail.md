7762cf49027d285d6b684614333e3f161ada05cc
Csilla Bessenyei <csilla@recrd.com>
2022-03-07 10:21:42 +0000

    Refactor cloudtrail config and disable force destroy

---
+++ bootstrap/cloudtrail.md
@@ -0,0 +1,6 @@
+# Cloudtrail
+
+Cloudtrail is enabled for all regions and logs to S3.
+
+# TODO
+ - Refactor cloudtrail config to a terraform module
--- bootstrap/cloudtrail.tf
+++ bootstrap/cloudtrail.tf
@@ -1,5 +1,55 @@
 data "aws_caller_identity" "current" {}
 
+
+data "aws_iam_policy_document" "allow_cloudtrail_access" {
+
+  statement {
+    sid = "AWSCloudTrailAclCheck20150319"
+
+    principals {
+      type        = "Service"
+      identifiers = ["cloudtrail.amazonaws.com"]
+    }
+
+    actions = ["s3:GetBucketAcl"]
+
+    resources = ["arn:aws:s3:::${local.cloudtrail_bucket_name}"]
+  }
+
+  statement {
+    sid = "AWSCloudTrailWrite20150319"
+
+    principals {
+      type        = "Service"
+      identifiers = ["cloudtrail.amazonaws.com"]
+    }
+
+    actions = ["s3:PutObject"]
+
+    resources = [
+      "arn:aws:s3:::${local.cloudtrail_bucket_name}/${local.s3_key_prefix}/AWSLogs/${data.aws_caller_identity.current.account_id}/*",
+    ]
+
+    condition {
+      test     = "StringEquals"
+      variable = "s3:x-amz-acl"
+
+      values = [
+        "bucket-owner-full-control",
+      ]
+    }
+
+    condition {
+      test     = "StringEquals"
+      variable = "AWS:SourceArn"
+
+      values = [
+        "arn:aws:cloudtrail:eu-west-1:${data.aws_caller_identity.current.account_id}:trail/management_trail",
+      ]
+    }
+  }
+}
+
 resource "aws_cloudtrail" "management_trail" {
   name = "management_trail"
 
@@ -7,7 +57,7 @@ resource "aws_cloudtrail" "management_trail" {
   is_multi_region_trail         = true
   enable_log_file_validation    = true
   s3_bucket_name                = aws_s3_bucket.recrd_cloudtrail.id
-  s3_key_prefix                 = "management"
+  s3_key_prefix                 = local.s3_key_prefix
 
   event_selector {
     read_write_type                  = "All"
@@ -16,10 +66,14 @@ resource "aws_cloudtrail" "management_trail" {
   }
 }
 
+resource "aws_s3_bucket_policy" "allow_access_from_another_account" {
+  bucket = aws_s3_bucket.recrd_cloudtrail.id
+  policy = data.aws_iam_policy_document.allow_cloudtrail_access.json
+}
+
 resource "aws_s3_bucket" "recrd_cloudtrail" {
   acl           = "private"
-  bucket        = "recrd-cloudtrail"
-  force_destroy = true
+  bucket        = local.cloudtrail_bucket_name
 
   server_side_encryption_configuration {
     rule {
@@ -32,37 +86,5 @@ resource "aws_s3_bucket" "recrd_cloudtrail" {
   versioning {
     enabled = false
   }
+}
 
-  # TODO: refactor to HCL
-  # At the same time add depends_on to trail resource to depend on policy_document creation to avoid transient issues 
-  policy = <<POLICY
-{
-    "Version": "2012-10-17",
-    "Statement": [
-        {
-            "Sid": "AWSCloudTrailAclCheck20150319",
-            "Effect": "Allow",
-            "Principal": {
-                "Service": "cloudtrail.amazonaws.com"
-            },
-            "Action": "s3:GetBucketAcl",
-            "Resource": "arn:aws:s3:::recrd-cloudtrail"
-        },
-        {
-            "Sid": "AWSCloudTrailWrite20150319",
-            "Effect": "Allow",
-            "Principal": {
-                "Service": "cloudtrail.amazonaws.com"
-            },
-            "Action": "s3:PutObject",
-            "Resource": "arn:aws:s3:::recrd-cloudtrail/AWSLogs/378942204220/*",
-            "Condition": {
-                "StringEquals": {
-                    "s3:x-amz-acl": "bucket-owner-full-control"
-                }
-            }
-        }
-    ]
-}
-  POLICY
-}
