554a1e6295584db356bc88bcd31e60e0271984f5
Csilla Bessenyei <100410058+csillabessenyei-recrd@users.noreply.github.com>
2022-03-24 10:04:00 +0000

    Revert ttl to 300 (#34)

--- bootstrap/dns_records_recrd_com.tf
+++ bootstrap/dns_records_recrd_com.tf
@@ -1,5 +1,5 @@
 locals {
-  ttl                = "60"
+  ttl                = "300"
   cloudfront_zone_id = "Z2FDTNDATAQYW2" # NOTE: This is the same string globally for all CloudFront aliases
 
   recrd_com_alias_a_records = {
