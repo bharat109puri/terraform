2a576d57c3696cf5992bfaa5b35068510612183f
Mate Farkas <mate@recrd.com>
2022-03-25 18:50:41 +0000

    Allow Riltech developers to list buckets

--- users/main.tf
+++ users/main.tf
@@ -12,9 +12,29 @@ module "iam_users" {
   ]
 
   third_parties = {
-    riltech = [
-      "mate+riltech@gmail.com",
-    ]
-    rokk = []
+    riltech = {
+      emails = [
+        "mate+riltech@gmail.com",
+      ]
+      policy_arns = [
+        aws_iam_policy.list_buckets.arn
+      ]
+    }
+    rokk = {
+      emails      = []
+      policy_arns = []
+    }
   }
+}
+
+data "aws_iam_policy_document" "list_buckets" {
+  statement {
+    actions   = ["s3:ListAllMyBuckets"]
+    resources = ["arn:aws:s3:::*"]
+  }
+}
+
+resource "aws_iam_policy" "list_buckets" {
+  name   = "list-buckets"
+  policy = data.aws_iam_policy_document.list_buckets.json
 }
