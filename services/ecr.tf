046391ca63da48fbe24b9b9ae77cdb098cf58e46
Mate Farkas <mate@recrd.com>
2022-05-03 15:38:29 +0000

    Add GitHub Actions role to each ECR repo

--- services/ecr.tf
+++ services/ecr.tf
@@ -1,23 +1,3 @@
-locals {
-  services = toset([
-    "angara",
-    "congo",
-    "danube",
-    "elba",
-    "hello-world",
-    "indus",
-    "nile",
-    "recrd-cli",
-  ])
-}
-
-data "tfe_outputs" "users" {
-  organization = "recrd"
-  workspace    = "users"
-}
-
-data "aws_caller_identity" "current" {}
-
 data "aws_iam_policy_document" "manage_ecr_repos" {
   statement {
     actions   = ["ecr:GetAuthorizationToken"]
@@ -25,19 +5,7 @@ data "aws_iam_policy_document" "manage_ecr_repos" {
   }
 
   statement {
-    actions = [
-      "ecr:BatchCheckLayerAvailability",
-      "ecr:BatchGetImage",
-      "ecr:CompleteLayerUpload",
-      "ecr:DescribeImages",
-      "ecr:DescribeRepositories",
-      "ecr:GetDownloadUrlForLayer",
-      "ecr:GetRepositoryPolicy",
-      "ecr:InitiateLayerUpload",
-      "ecr:ListImages",
-      "ecr:PutImage",
-      "ecr:UploadLayerPart",
-    ]
+    actions   = local.ecr_repo_actions
     resources = [for service in local.services : "arn:aws:ecr:${var.region}:${data.aws_caller_identity.current.account_id}:repository/${service}"]
   }
 }
@@ -56,7 +24,7 @@ resource "aws_ecr_repository" "services" {
 }
 
 resource "aws_iam_policy" "manage_ecr_repos" {
-  name   = "upload-ecr"
+  name   = "manage-ecr-repos"
   policy = data.aws_iam_policy_document.manage_ecr_repos.json
 }
 
---
+++ services/locals.tf
@@ -0,0 +1,26 @@
+locals {
+  ecr_repo_actions = [
+    "ecr:BatchCheckLayerAvailability",
+    "ecr:BatchGetImage",
+    "ecr:CompleteLayerUpload",
+    "ecr:DescribeImages",
+    "ecr:DescribeRepositories",
+    "ecr:GetDownloadUrlForLayer",
+    "ecr:GetRepositoryPolicy",
+    "ecr:InitiateLayerUpload",
+    "ecr:ListImages",
+    "ecr:PutImage",
+    "ecr:UploadLayerPart",
+  ]
+
+  services = toset([
+    "angara",
+    "congo",
+    "danube",
+    "elba",
+    "hello-world",
+    "indus",
+    "nile",
+    "recrd-cli",
+  ])
+}
