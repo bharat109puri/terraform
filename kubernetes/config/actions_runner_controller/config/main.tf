d614762245a806a828bd841e4dbc94efc166830f
Mate Farkas <mate@recrd.com>
2022-05-06 13:43:14 +0000

    Migrate actions-runner-controller into a separate state

--- kubernetes/config/actions_runner_controller/config/main.tf
+++ kubernetes/config/actions_runner_controller/config/main.tf
@@ -1,44 +1,6 @@
-resource "kubernetes_namespace_v1" "actions_runner_system" {
-  metadata {
-    name = "actions-runner-system"
-  }
-}
-
-# FIXME: Depends on CRDs
-resource "kubernetes_manifest" "controller_manager_sealed_secret" {
-  manifest = {
-    "apiVersion" = "bitnami.com/v1alpha1"
-    "kind"       = "SealedSecret"
-    "metadata" = {
-      "name"      = "controller-manager"
-      "namespace" = kubernetes_namespace_v1.actions_runner_system.metadata[0].name
-    }
-    "spec" = {
-      "encryptedData" = {
-        "github_app_id"              = "AgBg6eLC698WBqYZVkUVbiuxmNGkMJ21sSPuP4WbSsokcmaAijXNBPMV1qI0SddlAWyEmnL/i8Cs/LbO8DKOwHpiKQfi7MBahjmno5N07lrZ2bzjuh2+9KIHk1UTKspoE5VD0oBiNOOqCu1lV+Pmr8lWOe06WM6ZHqie2xSWwGnad6rQjxz3eG3fY09MjTM4dDLhv1mB2zrMQ2hHp81H4BemFcWtDyccIRElNK60tGXOMcFnofipapmvCI10W/vafH5IoUiW/nZEhB3RWmAgL97keIGLnnR5cpD2nPUo/pbIX+sgHUDOMT1nzpCgs/zEO07hUNMnDs2W0sE6t/6h+5/pcQbEq7vNhTJkIhSQXNmEPl026ZNx65z5T8mgvKa3zSOhOpMz34UQTuzRNRVoiZAe9tYYK17+7eWGrPsG7w8Z90z3e9MUmVjhvAgL6Vd6GTLYHLk4XzWdIYWc+855CfMPA+1iG8qiaSbduvK3qDd2QfEMqHSOVRoFRjd+3oF9ZtsVJeMJ6g14W7KDmO40N96iSBgBR/h9JcM9ZKqQHEuQvUmouUl1ehi/v5TkVzvJt4OYpMmb93+ziCH2YPmOIfSVBC3sjdvIt7u9nAm1KfAI0MXzdv0p7ADpC3dUXzFnNmb2zU9bH/1SPueDgOWQklssHacs8wYtEZ1saBY9oRnrK/NSrhOUTm2LW/SuyiXDnavVOO2giiE="
-        "github_app_installation_id" = "AgBIqrr68ZaT/1LlYqHqoCzmAmiJ8zHQi8zkIjiodG36ErJlJRTX17IKklYaIkSivEZ0pC6oT3KmNCMcl4UHHc4bpnnZvU93Rw3ygy/vWg70ASLfncxLW3nYFQT/+Xvw0HVrAHb+EGrqk901JjlymbnSMmjHnkUMH6qEARbjzSZjdFeEp81xHFylnh4ObDdQudIyUMZNI2yvo9/4gOHyVmF4p5kKnkgtlvPnrd2zL6brg3H7t1ChRpbww51VthYlX6XfkYenzwPTmjqIagO7zba19EWobnqnJOPTaARAVy1KJQyVNINNLUy7a+soTFxNYqvFlfaOPDYNDU02CO2OHfBh2lKq6/3HY/D6T11frxAPAxJWIA2tKlLMUEGFJ+HrEFs2ECILRhF9JvKax5TRBG/mCjU7n3bJDQBiKwBGqPJNtWUsMf67uXlGcHEwHOIFfRSptFCG8/4h9paTeb+IhFB+3FcV02bKjku7RILDc2PojP1EwzbxaP7+HAR5ieSpprQuL/mQsZeE70CuM8Q4UH75yFtrouEDLItyVa2vLakU6ngiOKQ/sCk+jUzXLyeV7v4h3imkxzRFrmGiuojROMbuLC2glA5Sr7QmwRGr4vPsNWR6f5NHHStUmP9w1GhSkA9neCXNrsskmCLLhHlWdqyn39XpzVKd0J0E/AHN1ft0TVvrpZsxUoinw+izlB26LBv/M8UfhObnpQ=="
-        "github_app_private_key"     = "AgBTTCySi8D5Suj4bHlBEIIhfHSCUnhzU/Cv/shs2lRjyT/SxsvSB3imN7AnFTPE3kFX9724fWa367zD5/mQWCJl37m+LaUiVSRLnzYBLD1Ti7yNju7g3wtkikxZnAGa5jajVbVxD5YnGibQmqBL+wQczG0qAhO11IwUB3Mz79qhEyOPPfxps8OKxKR79cwqj2Cq5Wry9e3OSz7sYvUJ7xuN91+nERCRlZ2UVhcIP1MQ9kuMp/pdnl1Jt6yrjZoweRmIA5n4EuN0RyEgl0dgRza+NswRtXLiwUe008qaQEM3TqX8vo72Cw6v0SgeRwuJz2U1Ga10RqQej4i7VXkP0BXbfb2WFtzTuyopMJhqlw7z1J+2VdNDko7CvUO0sbXloQ4dckaAovOXU1kK/mbWpHWNe8f/nNCATF+QjDiYA0+2sHUtfA4x06iSUhAx2B97toQK7n30WqgBqWQ6LqGFQeNhWAaZxVIxBTcygApj6jf9o4LtgJbH0/ZDNTQqsvZBraup113SqLbllO8jmlLcYAQ7yDluwpuPrOsVJbp/501n3dvLwVHHhmE73ZEnGxb6ShcZMfQlCtQlGaRHQYxuXCWcsRVNXab5nAtBrmDol815b5sdYqpa31Bgr/HU0iNMABRJMyCGrpnaf8f9cLFASMh7cj45mOV8lrVEvuQIZyPKF3D9zddLT0+bfcvY07Rg9lpoH3kGXh5GL4mMRTniMrmAteKsgmnEtOJcnqjbtKHXuVsN05kC5dCUVWS4K5IZkP2VlxuBVmhay6+lcc15OErhSoPrAsNHkirK37igS1ABGaThHQ0kqk6EOICd0qsmjCnCoMG10oitu0mIUAWdWnk+KC7RRliaJNZuPBLuI4+IiKqygDkwmbk636PnD5lRD235ss8EaavU67XDPzyETWwoYzzWkG/Qj+AFmRHic5IsYZnc8/WAOnJaDiEMkmk97K0ALGByqv/3nxrq6lOWyeo8iHqHBRNlGiojAyBCo5LDt/oAzDwGjMpG465n+7WpcwxtvdVxUxob/sH7R5vxlGlv4KNISI55shqXljE2mc5eNfQICtGrLGMH2wRyisYX1QxQH6S+f/4X/WxWqticC1+49e/ReRFYIbPHFn76hS0im1tbix7l1SqVPelorZf3woALq+59Iq75e/0RoHy80haTKuNk8FbJqbKmq5lFO0jkCQbuQhy6QWMcm5bE0BEYGI3Rhi20dn/5PB4Uxy7UcgxEvvJ3LY5DRTLJJjUnrdchbOgd9PyXZcdxtZr2BH/yoy48dAWweYPNeHsWM+wqPUOHxfvMYmf3sYLQIcYZJoABB/s0H8mAB/psbw9th9w+T74I7YgayjJW0IZ/S2E/Rrkmsr6rOhgrrGpyUb93qDY1RFAYtKLU4G7Iw9OGkpvIAZioUi4vu7dpx1W0ehRZLVtIXLhT27ejuay0bkpI8XNW3WeshyTIXFQZZHXxG0mJbTQVqwKKzrqH4G7Rl7OKG9HSHdxpTnRwAF6G3pFyIGfdovQOVSRjWRqONzkjssJNJGdAabbI0sXVP8g8TdRV6zDnbXKWDmRu6qvGX+t9OYG/LSwelit8vu3zLT7NDAKwQz+tlLMWTmIhYWm95Z1HTkkfvdrfjaTOqpvBmk9TCfnrrzXD7OH7djmtLV/a7Z0brxz6TOeC3jqcnwvjCDbJRIIAQuJf+5J76iMxI3RyFcuznrBc2H0bIJhLtyIOY63UhCp+chyMEONnDFTUiZzJxAvRpYFa4H3GAVE7Z39ueBS9SCqmGEiQjCSXjOibVUZuruRz0DD/mkrKDfswKCBI9dJskxS/1xURyVj7AckvzDU4Wew791nShSqPSgyGuGYOmDWBrtiRk6oXURhFLOycHJ3cxck4hE45502KcjfmMQaYjwCEdu1Td4co3a5JzEdnN9c9MRgla0b5XZpywTBpGXdFpb4K0/NlnUnsGGsKFEaLvrrDA9mRXLEuaj4mqh4dHOCR4QYqpOPH+bLN6ql04P6p0WdiDkUlg1bc47uRO5ObX/WV8t9MoEHsBzP7eVZwVljDZo+ACx6VRj525Rcsh0GP8AaUl8atTQadr1MwNB+BdxD4fAhHgnS6QBGekw982ETDLnOtR7h2JkOnCzGCVASXFwRhrPe3SJ7djlO69PXBx7bphr6U/BSk+M/g1oMujOgpZeeuYXuXDevvBOUvlSlxCzTw3qlNJ2kUPYFpkEZ/oIQAroMWohubZiSwkGfxVUo/Cua07icVwZxcSxCgdGdZmzSEH4lolF2w//16F+nBJ8MgxmrYVos3nVVHF0bW8RL+bp6TpJ77fTjPqM1vGrkUCg8wVPhvcrd3fJkZXpYvnobhJAP4DByIRJ7vWHlDYREBqXjUIXNpxiCocXOwrLH7lfI2ivWLe1OxVPNMDImLQk7kwvbQv1YPdvaoNhFPbyTLpOr39pt5BMyrfB/cnOCpXLhWlQa77PHW58zvKsYLzPxkr/Vig6A2NC/piw50VVQ3iEdxMw3M58bXssE8simBxCpDpShjDOuAc63Ys4chW7EFw5dwxx/I/S1HM/uEMfsLlnWws5aq6tAbleoZyMCK3ZtCnpTTiWXl0mLC7B2+bBil2XU3nZgzYqItJfG54h7KfZ2I3ziQ8oJSUX1hAhHdqAdhxgK8+FjmV4HMzLKS7hHOZSU4zOs7c08ujzGjvbXA9gUkq2FO0rvAzMJRZ9wJ4juq5yBlMlDqs8vtxuYTOH6XxQ7xnnnFf4HeBvld9hXTc5cfQpQimBS8gMJ54pt1GkBqIkU87tGl2OWhd3buFbSrlsLnwp8jQzc9RbP5B1lLCZhjwM7J3Fdo4f4OBALVkET3NnL2XmgaNtBfmilSvRHBDMeDQXp42pjl8Et1LPwPHuIBIlabZJ/cVXzR3UlNwb48MDIc8VggMTnsuFYcOZGpBITCdPZf0Gthm8aOMg=="
-      }
-    }
-  }
-
-  depends_on = [
-    helm_release.sealed_secrets_controller,
-  ]
-}
-
-resource "helm_release" "actions_runner_controller" {
-  name      = "actions-runner-controller"
-  namespace = kubernetes_namespace_v1.actions_runner_system.metadata[0].name
-
-  repository = "https://actions-runner-controller.github.io/actions-runner-controller"
-  chart      = "actions-runner-controller"
-  version    = "0.17.3"
-
-  depends_on = [
-    helm_release.cert_manager_controller,
-    kubernetes_manifest.controller_manager_sealed_secret,
-  ]
+data "tfe_outputs" "kubernetes_config" {
+  organization = "recrd"
+  workspace    = "kubernetes__config"
 }
 
 resource "kubernetes_service_account_v1" "github_actions_deployer" {
@@ -79,7 +41,7 @@ resource "kubernetes_role_binding_v1" "github_actions_deployer_deploy" {
   role_ref {
     api_group = "rbac.authorization.k8s.io"
     kind      = "Role"
-    name      = kubernetes_role_v1.deploy.metadata[0].name
+    name      = nonsensitive(data.tfe_outputs.kubernetes_config.values.deploy_role_name)
   }
 
   subject {
@@ -89,7 +51,6 @@ resource "kubernetes_role_binding_v1" "github_actions_deployer_deploy" {
   }
 }
 
-# FIXME: Depends on CRDs
 resource "kubernetes_manifest" "runner_deployment" {
   manifest = {
     "apiVersion" = "actions.summerwind.dev/v1alpha1"
@@ -128,15 +89,8 @@ resource "kubernetes_manifest" "runner_deployment" {
       }
     }
   }
-
-  depends_on = [
-    helm_release.actions_runner_controller,
-    kubernetes_cluster_role_binding_v1.github_actions_deployer_view,
-    kubernetes_role_binding_v1.github_actions_deployer_deploy,
-  ]
 }
 
-# FIXME: Depends on CRDs
 resource "kubernetes_manifest" "ssh_sealed_secret" {
   manifest = {
     "apiVersion" = "bitnami.com/v1alpha1"
@@ -152,8 +106,4 @@ resource "kubernetes_manifest" "ssh_sealed_secret" {
       }
     }
   }
-
-  depends_on = [
-    helm_release.sealed_secrets_controller,
-  ]
 }
---
+++ kubernetes/config/actions_runner_controller/config/variables.tf
@@ -0,0 +1,5 @@
+variable "tfe_token" {
+  description = "Terraform Cloud Team token"
+  type        = string
+  sensitive   = true
+}
