244ad3b0658f519c475a98d710cdbd943b9166f0
Bharat Puri <bhpuri@gmail.com>
2022-06-30 18:45:33 +0530

    Added condition for ge for provider version'

--- bootstrap/provider.tf
+++ bootstrap/provider.tf
@@ -2,11 +2,11 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "3.70.0"
+      version = ">= 3.70.0"
     }
     tfe = {
       source  = "hashicorp/tfe"
-      version = "0.28.1"
+      version = ">= 0.28.1"
     }
   }
 }
