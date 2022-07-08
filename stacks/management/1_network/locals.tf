fa414606236357183135e7b0cbdf434e7c265c0d
Bharat Puri <bhpuri@gmail.com>
2022-07-08 11:53:14 +0530

    UT-82 rollback vpc peering resources

--- stacks/management/1_network/locals.tf
+++ stacks/management/1_network/locals.tf
@@ -1,12 +1,12 @@
 locals {
-  ## VPC locals ##
   _all_cirds = cidrsubnets(var.vpc_cidr, 2, 2, 2, 4, 4, 4, 12, 12, 12, 6) # NOTE: /16 -> /18, /18, /18, /20, /20, /20, /28, /28, /28, /22
+
   private_cidrs = slice(local._all_cirds, 0, 3)
   public_cidrs  = slice(local._all_cirds, 3, 6)
   eks_cidrs     = slice(local._all_cirds, 6, 9)
   vpn_cidr      = local._all_cirds[9]
+
   dns_ip = cidrhost(var.vpc_cidr, 2)
+
   availability_zones = slice(sort(data.aws_availability_zones.available.names), 0, 3) # NOTE: Will fail if region has less than 3 availability zones
-  ### VPC peering locals ##
-  accepter_account_id = "${element(split(":", data.aws_vpc.accepter.arn), 4)}"
 }
