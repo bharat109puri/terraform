d61fc2b84ddf639813a254632507cc608cc56a77
Bharat Puri <bhpuri@gmail.com>
2022-07-11 12:20:11 +0530

    fix terraform format

--- modules/vpc-peering/data.tf
+++ modules/vpc-peering/data.tf
@@ -1,15 +1,15 @@
 data "aws_vpc" "accepter" {
   provider = aws.accepter
-  id = var.accepter_vpc_id
+  id       = var.accepter_vpc_id
 }
 data "aws_vpc" "owner" {
-    id = var.owner_vpc_id
+  id = var.owner_vpc_id
 }
 
 
 data "aws_route_tables" "accepter" {
   provider = aws.accepter
-  vpc_id = data.aws_vpc.accepter.id
+  vpc_id   = data.aws_vpc.accepter.id
 }
 
 data "aws_route_tables" "owner" {
