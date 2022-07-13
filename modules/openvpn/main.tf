6827a0d4d04ef9bd9e11a3c09b4e1c2dbb77a6be
Bharat Puri <bhpuri@gmail.com>
2022-07-13 15:49:26 +0530

    UT-85 added multiple ingress rules

--- modules/openvpn/main.tf
+++ modules/openvpn/main.tf
@@ -74,26 +74,33 @@ resource "aws_security_group" "openvpn_asg_sg" {
   description = "Security group for OpenVpn ASG"
   vpc_id      = var.vpc_id
 
-  ingress = [
-    {
-      from_port   = 443
-      to_port     = 443
-      protocol    = "tcp"
-      cidr_blocks = ["0.0.0.0/0"]
-    },
-    {
-      from_port   = 943
-      to_port     = 943
-      protocol    = "tcp"
-      cidr_blocks = ["0.0.0.0/0"]
-    },
-    {
-      from_port   = 1194
-      to_port     = 1194
-      protocol    = "udp"
-      cidr_blocks = ["0.0.0.0/0"]
-    },
-  ]
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = [var.vpc_cidr]
+  }
+
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 943
+    to_port     = 943
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 1194
+    to_port     = 1194
+    protocol    = "udp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 
   egress {
     from_port   = 0
