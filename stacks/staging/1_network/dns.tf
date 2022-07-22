4e11d96d18084c79f05f2f230aafa9d4a39c015b
Bharat Puri <bhpuri@gmail.com>
2022-07-22 13:54:20 +0530

    updated dns mapping with vars

--- stacks/staging/1_network/dns.tf
+++ stacks/staging/1_network/dns.tf
@@ -1,15 +1,20 @@
 # NOTE: `astra_database` resource URLs are using this domain
 resource "aws_route53_zone" "apps_astradb_datastax_com" {
-  name = "stg.apps.astra.datastax.com"
+  name = var.apps_astradb
 
   vpc {
     vpc_id = module.vpc.vpc_id
   }
+
+  vpc {
+    vpc_id = data.tfe_outputs.management.values.vpc_id
+  }
+
 }
 
 # NOTE: AstraDB secure bundle URLs are using this domain
 resource "aws_route53_zone" "db_astradb_datastax_com" {
-  name = "stg.db.astra.datastax.com"
+  name = var.db_astradb
 
   vpc {
     vpc_id = module.vpc.vpc_id
@@ -22,5 +27,5 @@ resource "aws_route53_zone" "db_astradb_datastax_com" {
 }
 
 resource "aws_route53_zone" "recrd_com" {
-  name = "stg.recrd.com"
+  name = var.recrd_zone
 }
