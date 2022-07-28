e8520c79272bf50f0e68688b5e88323cebe33b22
Bharat Puri <bhpuri@gmail.com>
2022-07-28 17:20:38 +0530

    added lena-batch-service

---
+++ stacks/staging/4_micro_services/lena-micro-service/main.tf
@@ -0,0 +1,22 @@
+data "tfe_outputs" "kubernetes" {
+  organization = "recrd"
+  workspace    = "kubernetes"
+}
+
+data "aws_iam_policy_document" "lena" {
+  statement {
+    actions   = ["sns:Publish"]
+    resources = ["*"]
+  }
+}
+
+module "lena_role" {
+  source = "git@github.com:RecrdGroup/terraform.git//modules/service_account_role?ref=master"
+
+  name      = "${var.environment}-lena-micro-services" # NOTE: ServiceAccount name to be used in k8s deployment
+  namespace = "default"
+
+  inline_policy = data.aws_iam_policy_document.lena.json
+
+  oidc_provider_arn = data.tfe_outputs.kubernetes.values.oidc_provider_arn
+}
