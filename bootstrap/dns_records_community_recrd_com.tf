60654a352aa15c4c0534a43118ad9a7e043bc0fe
Dmytro-Fesenko-92 <dmitryfesenko92@gmail.com>
2022-08-11 10:23:41 +0300

    updating mx records, updating images.recrd.com

--- bootstrap/dns_records_community_recrd_com.tf
+++ bootstrap/dns_records_community_recrd_com.tf
@@ -22,20 +22,20 @@
 #   records = ["tc5oqtf6elklp2n2c277jgotwrxv2vcu.dkim.amazonses.com"]
 # }
 
-# resource "aws_route53_record" "community_recrd_com_MX" {
-#   zone_id = aws_route53_zone.community_recrd_com.zone_id
-#   name    = "community.recrd.com"
-#   type    = "MX"
-#   ttl     = local.ttl
-#   records = [
-#     "1 ASPMX.L.GOOGLE.COM.",
-#     "5 ALT1.ASPMX.L.GOOGLE.COM.",
-#     "5 ALT2.ASPMX.L.GOOGLE.COM.",
-#     "10 ALT3.ASPMX.L.GOOGLE.COM.",
-#     "10 ALT4.ASPMX.L.GOOGLE.COM.",
-#     "10 inbound-smtp.us-west-2.amazonaws.com"
-#   ]
-# }
+resource "aws_route53_record" "community_recrd_com_MX" {
+  zone_id = aws_route53_zone.community_recrd_com.zone_id
+  name    = "community.recrd.com"
+  type    = "MX"
+  ttl     = local.ttl
+  records = [
+    "1 ASPMX.L.GOOGLE.COM.",
+    "5 ALT1.ASPMX.L.GOOGLE.COM.",
+    "5 ALT2.ASPMX.L.GOOGLE.COM.",
+    "10 ALT3.ASPMX.L.GOOGLE.COM.",
+    "10 ALT4.ASPMX.L.GOOGLE.COM.",
+    # "10 inbound-smtp.us-west-2.amazonaws.com"
+  ]
+}
 
 resource "aws_route53_record" "community_recrd_com_TXT" {
   zone_id = aws_route53_zone.community_recrd_com.zone_id
