ab6417823dfc63f6f9def5c602255adeeb212bb0
Dmytro-Fesenko-92 <dmitryfesenko92@gmail.com>
2022-08-13 16:50:56 +0300

    updating community.recrd.com

--- bootstrap/dns.tf
+++ bootstrap/dns.tf
@@ -20,6 +20,7 @@ resource "aws_route53_zone" "recrd_com" {
   name = "recrd.com"
 }
 
-resource "aws_route53_zone" "community_recrd_com" {
-  name = "community.recrd.com"
-}
+#NOTE: When we creating subdomain at google workspace and mapping it to separete hosted zone google unable to verify and activate Gmail
+# resource "aws_route53_zone" "community_recrd_com" {
+#   name = "community.recrd.com"
+# }
--- bootstrap/dns_records_community_recrd_com.tf
+++ bootstrap/dns_records_community_recrd_com.tf
@@ -1,29 +1,29 @@
-# resource "aws_route53_record" "_7mabyzrev6dgwagpayuaaqtpnhenkg4p_CNAME" {
-#   zone_id = aws_route53_zone.community_recrd_com.zone_id
-#   name    = "7mabyzrev6dgwagpayuaaqtpnhenkg4p._domainkey.community.recrd.com"
-#   type    = "CNAME"
-#   ttl     = local.ttl
-#   records = ["7mabyzrev6dgwagpayuaaqtpnhenkg4p.dkim.amazonses.com"]
-# }
-#
-# resource "aws_route53_record" "kn2zo4mtftthik5cz2h5ncrcllabslic_CNAME" {
-#   zone_id = aws_route53_zone.community_recrd_com.zone_id
-#   name    = "kn2zo4mtftthik5cz2h5ncrcllabslic._domainkey.community.recrd.com"
-#   type    = "CNAME"
-#   ttl     = local.ttl
-#   records = ["kn2zo4mtftthik5cz2h5ncrcllabslic.dkim.amazonses.com"]
-# }
-#
-# resource "aws_route53_record" "tc5oqtf6elklp2n2c277jgotwrxv2vcu_CNAME" {
-#   zone_id = aws_route53_zone.community_recrd_com.zone_id
-#   name    = "tc5oqtf6elklp2n2c277jgotwrxv2vcu._domainkey.community.recrd.com"
-#   type    = "CNAME"
-#   ttl     = local.ttl
-#   records = ["tc5oqtf6elklp2n2c277jgotwrxv2vcu.dkim.amazonses.com"]
-# }
+resource "aws_route53_record" "_7mabyzrev6dgwagpayuaaqtpnhenkg4p_CNAME" {
+  zone_id = aws_route53_zone.recrd_com.zone_id
+  name    = "7mabyzrev6dgwagpayuaaqtpnhenkg4p._domainkey.community.recrd.com"
+  type    = "CNAME"
+  ttl     = local.ttl
+  records = ["7mabyzrev6dgwagpayuaaqtpnhenkg4p.dkim.amazonses.com"]
+}
+
+resource "aws_route53_record" "kn2zo4mtftthik5cz2h5ncrcllabslic_CNAME" {
+  zone_id = aws_route53_zone.recrd_com.zone_id
+  name    = "kn2zo4mtftthik5cz2h5ncrcllabslic._domainkey.community.recrd.com"
+  type    = "CNAME"
+  ttl     = local.ttl
+  records = ["kn2zo4mtftthik5cz2h5ncrcllabslic.dkim.amazonses.com"]
+}
+
+resource "aws_route53_record" "tc5oqtf6elklp2n2c277jgotwrxv2vcu_CNAME" {
+  zone_id = aws_route53_zone.recrd_com.zone_id
+  name    = "tc5oqtf6elklp2n2c277jgotwrxv2vcu._domainkey.community.recrd.com"
+  type    = "CNAME"
+  ttl     = local.ttl
+  records = ["tc5oqtf6elklp2n2c277jgotwrxv2vcu.dkim.amazonses.com"]
+}
 
 resource "aws_route53_record" "community_recrd_com_MX" {
-  zone_id = aws_route53_zone.community_recrd_com.zone_id
+  zone_id = aws_route53_zone.recrd_com.zone_id
   name    = "community.recrd.com"
   type    = "MX"
   ttl     = local.ttl
@@ -33,25 +33,19 @@ resource "aws_route53_record" "community_recrd_com_MX" {
     "5 ALT2.ASPMX.L.GOOGLE.COM.",
     "10 ALT3.ASPMX.L.GOOGLE.COM.",
     "10 ALT4.ASPMX.L.GOOGLE.COM.",
-    # "10 inbound-smtp.us-west-2.amazonaws.com"
+    "10 inbound-smtp.us-west-2.amazonaws.com"
   ]
 }
 
 resource "aws_route53_record" "community_recrd_com_TXT" {
-  zone_id = aws_route53_zone.community_recrd_com.zone_id
+  zone_id = aws_route53_zone.recrd_com.zone_id
   name    = "community.recrd.com"
   type    = "TXT"
   ttl     = local.ttl
   records = [
     "google-site-verification=6O1qtYDKr12JtHKy5-x7gcZCdj5pJxvqA1WxrXvBIEk",
-    #"v=spf1 include:amazonses.com ~all"
+    "v=spf1 include:amazonses.com ~all"
   ]
 }
 
-resource "aws_route53_record" "_tw2draljw73g_CNAME" {
-  zone_id = aws_route53_zone.community_recrd_com.zone_id
-  name    = "tw2draljw73g.community.recrd.com."
-  type    = "CNAME"
-  ttl     = local.ttl
-  records = ["gv-irx2zti6nqcibz.dv.googlehosted.com"]
-}
+#NOTE: When we creating subdomain at google workspace and mapping it to separete hosted zone google unable to verify and activate Gmail
