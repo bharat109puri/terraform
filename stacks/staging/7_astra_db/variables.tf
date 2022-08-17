a51bd915a6c3c0cecfdb4ae253d07cbf0e2521e1
Bharat Puri <bhpuri@gmail.com>
2022-08-17 15:05:49 +0530

    updated dir structures

--- stacks/staging/7_astra_db/variables.tf
+++ stacks/staging/7_astra_db/variables.tf
@@ -2,7 +2,6 @@ variable "astra_token" {
   description = "DataStax Astra API Administrator Service Account token"
   type        = string
   sensitive   = true
-  default     = "AstraCS:LEUCshQGwMZSmycmGdELTuDQ:5c010f96654e44230c8c228b20ae58b9f201b2e3419c9b46f9845955dcac7755"
 }
 
 variable "region" {
@@ -19,5 +18,4 @@ variable "tfe_token" {
   description = "Terraform Cloud Team token"
   type        = string
   sensitive   = true
-  default     = "SynI2fjDrXCngw.atlasv1.J7n4SYfp81d0ADdxj0lKbuiFtUL2fb9R2neMgIXDJT5QPezPjgnoA5NsT2xfzXJYwrU"
 }
