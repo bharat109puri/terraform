505d4a9d9f2700d12cc50657977c50115fa6f957
Bharat Puri <bhpuri@gmail.com>
2022-10-17 19:55:31 +0530

    updated config  rule precedence

--- stacks/prod/5_amplify/main.tf
+++ stacks/prod/5_amplify/main.tf
@@ -54,17 +54,16 @@ resource "aws_amplify_app" "portal" {
   #    },
   #  ]
 
+  custom_rule {
+    source = "/<*>"
+    target = "/index.html"
+    status = "404"
+  }
 
   custom_rule {
     source = "</^[^.]+$|\\.(?!(css|gif|ico|jpg|js|png|txt|svg|woff|ttf|map|json)$)([^.]+$)/>"
     target = "/index.html"
     status = "200"
-  }
-
-  custom_rule {
-    source = "/<*>"
-    target = "/index.html"
-    status = "404"
   }
 
 }
