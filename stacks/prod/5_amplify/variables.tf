9249319c3fb0daccfa62a251c4e6ef3af6765299
Bharat Puri <bhpuri@gmail.com>
2022-10-19 11:35:06 +0530

    removed default vars of token

--- stacks/prod/5_amplify/variables.tf
+++ stacks/prod/5_amplify/variables.tf
@@ -7,6 +7,5 @@ variable "region" {
 variable "tfe_token" {
   description = "Terraform Cloud Team token"
   type        = string
-  default     = "SynI2fjDrXCngw.atlasv1.J7n4SYfp81d0ADdxj0lKbuiFtUL2fb9R2neMgIXDJT5QPezPjgnoA5NsT2xfzXJYwrU"
   sensitive   = true
 }
